11ea3df224cd133068597ccd5d01bcce2d206607
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-11-16 21:37:42 -0300

    cria bot ou hacker 2 e organiza código

--- src/App.java
+++ src/App.java
@@ -5,16 +5,19 @@ import cartas.Ataque;
 import cartas.Carta;
 import cartas.Defesa;
 import cartas.Suporte;
+import jogadores.Bot;
 import jogadores.Hacker;
 import jogadores.Jogador;
 import leituraDeArquivos.Leitor;
 
 public class App {
     public static void main(String[] args) throws Exception{
+        //lê e cria as listas
         List<Ataque> ataques = Leitor.listaDeAtaques();
         List<Defesa> defesas = Leitor.listaDeDefesas();
-        List<Suporte> suportes = Leitor.listaDeSuportes(); //lê e cria as listas
+        List<Suporte> suportes = Leitor.listaDeSuportes();
 
+        //realiza a leitura do modo de jogo
         Scanner leitura = new Scanner(System.in);
         System.out.println("Escolha o modo de jogo:");
         System.out.println("1. Hacker vs Bot");
@@ -22,24 +25,24 @@ public class App {
         int modoDeJogo = leitura.nextInt(); //passa o valor lido para modoDeJogo
         leitura.nextLine();
         
+        //criação do hacker 1
         System.out.println("Hacker 1 - Escolha seu nome: ");
         String nome = leitura.nextLine();
         System.out.println("Hacker 1 - Escolha seu ID: ");
         int id = leitura.nextInt();
         leitura.nextLine();
-
         Hacker hacker1 = new Hacker(nome, id);   
         System.out.println(hacker1.getNome());
         System.out.println(hacker1.getId());
 
+        //criação da mão do hacker 1
         System.out.println("Hacker 1 - Faça sua escolha: ");
         System.out.println("1. Desejo cartas aleatórias");
         System.out.println("2. Desejo escolher minhas cartas");
         int desejo = leitura.nextInt();
-        leitura.nextLine();
+        leitura.nextLine(); //? transformar em função ?
 
         List<Carta> maoHacker1;
-
         if(desejo == 1){
             maoHacker1 = Jogador.selecionaCartas(leitura, ataques, defesas, suportes, true);
         }
@@ -47,10 +50,36 @@ public class App {
             maoHacker1 = Jogador.selecionaCartas(leitura, ataques, defesas, suportes, false);
         }
 
-        System.out.println("Mão do Hacker 1: ");
-        for (int i = 0; i < maoHacker1.size(); i++) {
-            Carta carta = maoHacker1.get(i);
-            System.out.println((i+1) + ". " + carta.getNome() + " - Tipo: " + carta.getTipo() + " - Poder: " + carta.getPoder() + " - Custo: " + carta.getCusto());
+        //verifica se jogo é contra bot ou outro hacker
+        if(modoDeJogo == 1){
+            Bot bot = new Bot();
+            List<Carta> maoBot = Jogador.selecionaCartas(leitura, ataques, defesas, suportes, true);
+        }
+        else{
+            //criação do hacker 2
+            System.out.println("Hacker 2 - Escolha seu nome: ");
+            nome = leitura.nextLine();
+            System.out.println("Hacker 2 - Escolha seu ID: ");
+            id = leitura.nextInt();
+            leitura.nextLine();
+            Hacker hacker2 = new Hacker(nome, id);   
+            System.out.println(hacker2.getNome());
+            System.out.println(hacker2.getId());
+
+            //criação da mão do hacker 2
+            System.out.println("Hacker 2 - Faça sua escolha: ");
+            System.out.println("1. Desejo cartas aleatórias");
+            System.out.println("2. Desejo escolher minhas cartas");
+            desejo = leitura.nextInt();
+            leitura.nextLine();
+
+            List<Carta> maoHacker2;
+            if(desejo == 2){
+                maoHacker1 = Jogador.selecionaCartas(leitura, ataques, defesas, suportes, true);
+            }
+            else {
+                maoHacker2 = Jogador.selecionaCartas(leitura, ataques, defesas, suportes, false);
+            }
         }
     }
 }
