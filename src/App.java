a6486310a16bca0d3b3dff28c4456b5042360a53
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2025-11-18 10:36:23 -0300

    simplica código (adiciona funções para evitar repetições)

--- src/App.java
+++ src/App.java
@@ -6,9 +6,11 @@ import cartas.Defesa;
 import cartas.Suporte;
 import jogadores.Bot;
 import jogadores.Hacker;
+import jogadores.Jogador;
 import leituraDeArquivos.Leitor;
 
 public class App {
+    static Scanner leitura = new Scanner(System.in);
     public static void main(String[] args) throws Exception{
         //lê e cria as listas
         List<Ataque> ataques = Leitor.listaDeAtaques();
@@ -16,36 +18,14 @@ public class App {
         List<Suporte> suportes = Leitor.listaDeSuportes();
 
         //realiza a leitura do modo de jogo
-        Scanner leitura = new Scanner(System.in);
         System.out.println("Escolha o modo de jogo:");
         System.out.println("1. Hacker vs Bot");
         System.out.println("2. Hacker vs Hacker");
-        int modoDeJogo = leitura.nextInt(); //passa o valor lido para modoDeJogo
-        leitura.nextLine();
+        int modoDeJogo = lerInteiro(); //passa o valor lido para modoDeJogo
         
         //criação do hacker 1
-        System.out.println("Hacker 1 - Escolha seu nome: ");
-        String nome = leitura.nextLine();
-        System.out.println("Hacker 1 - Escolha seu ID: ");
-        int id = leitura.nextInt();
-        leitura.nextLine();
-        Hacker hacker1 = new Hacker(nome, id);   
-        System.out.println("Hacker 1: " + hacker1.getNome());
-        System.out.println("ID: " + hacker1.getId());
-
-        //criação da mão do hacker 1
-        System.out.println("Hacker 1 - Faça sua escolha: ");
-        System.out.println("1. Desejo cartas aleatórias");
-        System.out.println("2. Desejo escolher minhas cartas");
-        int desejo = leitura.nextInt();
-        leitura.nextLine(); //? transformar em função ?
-
-        if(desejo == 1){
-            hacker1.selecionaCartas(leitura, ataques, defesas, suportes, true);
-        }
-        else {
-            hacker1.selecionaCartas(leitura, ataques, defesas, suportes, false);
-        }
+        Hacker hacker1 = criarHacker(1);
+        escolhaDeCartas(hacker1, ataques, defesas, suportes);
 
         //verifica se jogo é contra bot ou outro hacker
         if(modoDeJogo == 1){
@@ -54,28 +34,46 @@ public class App {
         }
         else{
             //criação do hacker 2
-            System.out.println("Hacker 2 - Escolha seu nome: ");
-            nome = leitura.nextLine();
-            System.out.println("Hacker 2 - Escolha seu ID: ");
-            id = leitura.nextInt();
-            leitura.nextLine();
-            Hacker hacker2 = new Hacker(nome, id);   
-            System.out.println(hacker2.getNome());
-            System.out.println(hacker2.getId());
-
-            //criação da mão do hacker 2
-            System.out.println("Hacker 2 - Faça sua escolha: ");
-            System.out.println("1. Desejo cartas aleatórias");
-            System.out.println("2. Desejo escolher minhas cartas");
-            desejo = leitura.nextInt();
-            leitura.nextLine();
-
-            if(desejo == 1){
-                hacker2.selecionaCartas(leitura, ataques, defesas, suportes, true);
-            }
-            else {
-                hacker2.selecionaCartas(leitura, ataques, defesas, suportes, false);
-            }
+            Hacker hacker2 = criarHacker(2);
+            escolhaDeCartas(hacker2, ataques, defesas, suportes);
         }
+    }
+
+    //método para criar os hackers
+    public static Hacker criarHacker(int n){
+        System.out.println("Hacker " + n);
+        
+        System.out.print("Escolha seu nome: ");
+        String nome = leitura.nextLine();
+        
+        System.out.print("Escolha seu ID: ");
+        int id = lerInteiro();
+
+        Hacker hacker = new Hacker(nome, id);
+        System.out.println("Hacker criado: " + hacker.getNome() + " (ID: " + hacker.getId() + ")");
+        return hacker;
+    }
+
+    //método para decidir se a escolha de cartas é aleatória ou não
+    public static void escolhaDeCartas(Jogador jogador, List<Ataque> atq, List<Defesa> def, List<Suporte> sup){
+        System.out.println(jogador.getNome() + " faça sua escolha de cartas:");
+        System.out.println("1. Desejo cartas aleatórias");
+        System.out.println("2. Desejo escolher minhas cartas");
+        
+        int desejo = lerInteiro();
+
+        if (desejo == 1){
+            jogador.selecionaCartas(leitura, atq, def, sup, true);
+        }
+        else{
+            jogador.selecionaCartas(leitura, atq, def, sup, false);
+        }
+    }
+
+    //método para ler inteiros
+    public static int lerInteiro(){
+        int valor = leitura.nextInt();
+        leitura.nextLine();
+        return valor;
     }
 }
